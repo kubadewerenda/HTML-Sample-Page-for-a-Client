d69109fd1116f8c2baab7b2063dc366c410a4501
kubadewerenda <kubadewerendaa@gmail.com.com>
2025-10-13 19:47:41 +0200

    v2

--- components/Header/header.php
+++ components/Header/header.php
@@ -2,7 +2,7 @@
     $current = basename($_SERVER['SCRIPT_NAME']);
     $isActive = fn($files) => in_array($current, (array)$files) ? 'active' : '';
 ?>
-<header class="container-fluid">
+<header class="container-fluid hero">
     <h1 class="container-h1">
         <img class="diamond-napis" src="/css/img/diamond.png" alt="Diamond Care">
         <img class="diamond-logo" src="/css/img/logo.png" alt="Diamond Care logo">
@@ -10,7 +10,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark margines">
         <a class="navbar-brand" href="/index.php">
-        <img src="/css/img/logo.png" width="60" height="40" alt="Diamond Care" class="d-inline-block align-bottom">
+        <img src="/css/img/logo.png"  alt="Diamond Care" class="header-sm-logo d-inline-block align-bottom">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainmenu"
                 aria-controls="mainmenu" aria-expanded="false" aria-label="Przełącznik nawigacji">
@@ -22,13 +22,7 @@
             <li class="nav-item">
             <a class="nav-link nav-underline <?= $isActive(['index.php']) ?>" href="/index.php">Home</a>
             </li>
-
-            <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle nav-underline <?= $isActive(['Detailing.php','Akcesoria.php']) ?>" href="#" data-bs-toggle="dropdown" role="button" aria-expanded="false" id="submenu" aria-haspopup="true">Oferty</a>
-            <div class="dropdown-menu" aria-labelledby="submenu">
-                <a class="dropdown-item <?= $isActive(['Detailing.php']) ?>" href="/Detailing.php">Detailing</a>
-            </div>
-            </li>
+            <li class="nav-item"><a class="nav-link nav-underline <?= $isActive(['Detailing.php']) ?>" href="/Detailing.php">Oferty</a></li>
             <li class="nav-item"><a class="nav-link nav-underline <?= $isActive(['Cennik.php']) ?>" href="/Cennik.php">Cennik</a></li>
             <li class="nav-item"><a class="nav-link nav-underline <?= $isActive(['About.php']) ?>" href="/About.php">O nas</a></li>
             <li class="nav-item"><a class="nav-link nav-underline <?= $isActive(['Efekty.php']) ?>" href="/Efekty.php">Realizacje</a></li>
